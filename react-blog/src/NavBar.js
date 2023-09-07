4ab330dbb8496ffe7a1019febff386727cd39818
DrOneMaloney <maloneyjcarter@gmail.com>
2023-09-07 18:43:52 -0500

    Removed the file  for the icons. Finished(for now) the NavBar setup.

--- react-blog/src/NavBar.js
+++ react-blog/src/NavBar.js
@@ -1,28 +1,23 @@
+// NavBar.js
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from "bootstrap";
-
-
 
 function NavBar() {
     return (
-        
         <nav className="nav-header">
             <ul className="nav-list">
-                <button className="nav-buttons">
-                    <li><Link to="/" className="nav-buttons">Home</Link></li>                
-                </button>
-                <button className="nav-buttons">
-                    <li><Link to="/projects" className="nav-buttons">Projects</Link></li>
-                </button>
-                <button className="nav-buttons">
-                    <li><Link to="/about" className="nav-buttons">About Me</Link></li>
-                </button>
-                <button>
-                    <i className="material-icons">notifications</i>
-                </button>
+                <li>
+                    <Link to="/" className="nav-buttons">
+                        <i className="material-icons">home</i>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/projects" className="nav-buttons">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/about" className="nav-buttons">About Me</Link>
+                </li>
             </ul>
         </nav>
     );
